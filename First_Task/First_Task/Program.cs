056952684c93faffd527b3c465f4a673ab35ceca
Yuriy Plyska <YuriyPly@gmail.com>
2019-08-13 18:14:21 +0300

    added Console.ReadLine

--- First_Task/First_Task/Program.cs
+++ First_Task/First_Task/Program.cs
@@ -15,7 +15,26 @@ namespace First_Task
         
         static void Main(string[] args)
         {
-            Program ob = new Program(5, 10, 8, 5);
+            Console.WriteLine( "Type the first number" );
+            string first = Console.ReadLine();
+            uint f = 0;
+            UInt32.TryParse(first, out f);
+            Console.WriteLine("Type the second number");
+            string second = Console.ReadLine();
+            uint sec = 0;
+            UInt32.TryParse(second, out sec);
+            Console.WriteLine("Type the third number bigger then first");
+            string third = Console.ReadLine();
+            uint th = 0;
+            UInt32.TryParse(third, out th);
+            Console.WriteLine("Type the force number less then second");
+            string force = Console.ReadLine();
+            uint fo = 0;
+            UInt32.TryParse(force, out fo);
+
+
+
+            Program ob = new Program(f, sec, th, fo);
             uint length = ob.Bx - ob.Ax;
             uint width = ob.Ay - ob.By;
             uint perim = Perimeter(ob);
@@ -30,7 +49,7 @@ namespace First_Task
             Bx = bx;
             By = by;
 
-            if ((Ax >= Bx) && (By <= By))
+            if ((Ax >= Bx) && (By <= Ay))
             {
                 Console.WriteLine("ax has to be less than bx and ay -bigger than by");
                 Console.ReadKey();
