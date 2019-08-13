1755f38abe280ae421aa46be40ad951e766fe2a6
Yuriy Plyska <Yuriyply@gmail.com>
2019-08-13 11:28:47 +0300

    Update Program.cs

--- First_Task/First_Task/Program.cs
+++ First_Task/First_Task/Program.cs
@@ -30,7 +30,7 @@ namespace First_Task
             Bx = bx;
             By = by;
 
-            if ((Ax >= Bx) && (By <= By))
+            if ((Ax >= Bx) && (By <= Ay))
             {
                 Console.WriteLine("ax has to be less than bx and ay -bigger than by");
                 Console.ReadKey();
