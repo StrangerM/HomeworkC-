6c06387c2becc42cacd0379b856f10b12fb3f9cc
Yuriy Plyska <YuriyPly@gmail.com>
2019-08-26 16:41:17 +0300

    added Task2_1

---
+++ Task2_1/Task2_1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2_1
+{
+    class Figure
+    {
+        static void Main(string[] args)
+        {
+        }
+        public virtual void Draw()
+        {
+            Console.WriteLine( "It is Figure clas");
+        }
+    }
+    class Square : Figure
+    {
+        public override void Draw()
+        {
+            Console.WriteLine(  "It is Square class");
+        }
+
+    }
+    class Rectangle : Figure
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("It is Rectangle class");
+        }
+    }
+}
