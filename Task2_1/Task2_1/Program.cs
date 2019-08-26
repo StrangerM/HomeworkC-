d0e9f031475e7de0135c7071c6a32975178500d8
Yuriy Plyska <YuriyPly@gmail.com>
2019-08-26 17:26:21 +0300

    added second and third task

--- Task2_1/Task2_1/Program.cs
+++ Task2_1/Task2_1/Program.cs
@@ -8,10 +8,21 @@ namespace Task2_1
 {
    abstract class Figure
     {
+        readonly uint X;
+        readonly uint Y;
+        public Figure(uint x, uint y)
+        {
+            X = x;
+            Y = y;
+        }
+
         static void Main(string[] args)
         {
         }
-        public abstract void Draw();
+        public virtual void Draw()
+        {
+            Console.WriteLine(   "It is Figure class" );
+        }
         
            
         
@@ -20,12 +31,20 @@ namespace Task2_1
     {
         public override void Draw()
         {
-            Console.WriteLine(  "It is Square class");
+            Console.WriteLine("It is Squer class");
+        }
+        public Square(uint x, uint y) : base( x,  y)
+        {
+            
+
         }
 
     }
     class Rectangle : Figure
     {
+        public Rectangle(uint x, uint y) : base(x,y)
+        {
+        }
         public override void Draw()
         {
             Console.WriteLine("It is Rectangle class");
