c8c29ceb4fc4396b7ae44aa47d05a10d34854fb6
Yuriy Plyska <YuriyPly@gmail.com>
2019-08-26 18:25:26 +0300

    added the last task

--- Task2_1/Task2_1/Program.cs
+++ Task2_1/Task2_1/Program.cs
@@ -6,46 +6,65 @@ using System.Threading.Tasks;
 
 namespace Task2_1
 {
-   abstract class Figure
+
+    interface IDrawable
+    {
+        void Draw();
+        
+    }
+     class Figure : IDrawable
     {
         readonly uint X;
         readonly uint Y;
+       
+
         public Figure(uint x, uint y)
         {
             X = x;
             Y = y;
         }
-
+        public Figure() { }
         static void Main(string[] args)
         {
+            IDrawable[] arr = new IDrawable[3];
+            DrawAll(new Figure(), new Square(), new Rectangle());
+            Console.ReadKey();
         }
-        public virtual void Draw()
+        public  void Draw()
         {
             Console.WriteLine(   "It is Figure class" );
         }
+        public static void DrawAll(params IDrawable[] arr)
+        {
+
+
+            arr[0] = new Figure();
+            arr[1] = new Square();
+            arr[2] = new Rectangle();
+            arr[0].Draw();
+            arr[1].Draw();
+            arr[2].Draw();
         
+          
+        }
            
         
     }
-    class Square : Figure
+    class Square : IDrawable
     {
-        public override void Draw()
+      public void Draw()
         {
             Console.WriteLine("It is Squer class");
         }
-        public Square(uint x, uint y) : base( x,  y)
-        {
-            
-
-        }
+       
+        public Square() { }
 
     }
-    class Rectangle : Figure
+    class Rectangle : IDrawable
     {
-        public Rectangle(uint x, uint y) : base(x,y)
-        {
-        }
-        public override void Draw()
+        
+        public Rectangle() { }
+        public  void Draw()
         {
             Console.WriteLine("It is Rectangle class");
         }
